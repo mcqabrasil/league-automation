1334c8525468b609f4b3b0b415c8a4b3b14695b1
Gabriel Viana Volpato Pacher <gpacher@mcfadyen.com>
2020-03-09 10:32:57 -0300

    Footer: Second commit on footer tests

--- app/assertions/footer_assert.rb
+++ app/assertions/footer_assert.rb
@@ -6,33 +6,27 @@ class FooterAssertion
   
     def initialize
       @footer = FooterMethods.new
-      @login = LoginMethods.new
     end
   end
 
 
   def assert_shipping_link_works
-    @footer.click_shipping
     expect(page).to have_current_path('/pages/shipping-payments')
   end
 
   def assert_returns_link_works
-    @footer.click_returns
     expect(page).to have_current_path('/pages/returns')
   end
 
   def assert_terms_cond_link_works
-    @footer.click_tac
     expect(page).to have_current_path('/pages/terms-and-conditions')
   end
 
   def assert_privacy_link_works
-    @footer.click_privacy
     expect(page).to have_current_path('/pages/privacy')
   end
 
   def assert_contact_us_link_works
-    @footer.click_contact_us
     expect(page).to have_current_path('/pages/contact-us')
   end
   
--- spec/footer_spec.rb
+++ spec/footer_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../app/pages/page methods/mega-menu_met'
+require_relative '../app/assertions/footer_met'
 require_relative '../app/assertions/footer_assert'
 
 
@@ -9,6 +10,7 @@ describe '', :LEAGUE76 do
     @header.click_my_account
     @logon.login('akratz@mcfadyen.com', 'Admin123') # info provisória
     visit '/'
+    @footer_met = FooterMethods.new
     @footer_as = FooterAssertions.new
   end
 
@@ -23,18 +25,23 @@ describe '', :LEAGUE76 do
 
   context "Verify Footer links functionalities", :LEAGUE76_step1 do
     it 'User is redirected to the "Shipping & Payment" informations page', :step_1 do
+      @footer_met.click_shipping
       @footer_as.assert_shipping_link_works
     end
     it 'User is redirected to the "Returns" informations works', :step2 do
+      @footer_met.click_shipping
       @footer_as.assert_returns_link_works
     end
     it 'User is redirected to the "Terms and Conditions" informations page', :step3 do
+      @footer_met.click_shipping
       @footer_as.assert_terms_cond_link_works
     end
     it 'User is redirected to the "Privacy" informations page', :step4 do
+      @footer_met.click_shipping
       @footer_as.assert_privacy_link_works
     end
     it 'User is redirected to the "Contact Us" informations page', :step5 do
+      @footer_met.click_shipping
       @footer_as.assert_contact_us_link_works
     end
 
